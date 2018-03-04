3d0d054cacaef7accaf921b943966e4b59b63df5
liuman <635549913@qq.com>
2018-03-04 15:27:40 +0800

    用户类增加了删除、添加、列表方法

---
+++ class/user.class.php
@@ -0,0 +1,30 @@
+<?php 
+/**
+* 用户类
+*/
+class User
+{
+	
+	// 显示用户列表
+	public function index()
+	{
+
+	}
+
+
+	// 添加用户
+	public function add()
+	{
+
+	}
+
+
+	// 删除用户
+	public function del()
+	{
+
+	}
+
+
+
+}//c
